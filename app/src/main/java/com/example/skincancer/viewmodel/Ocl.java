6f65a9255fc90f6607c2ab4203672e41065f0faa
Lyan-bit <72354386+Lyan-bit@users.noreply.github.com>
2023-06-09 15:07:58 +0100

    Update Ocl.java

--- app/src/main/java/com/example/skincancer/viewmodel/Ocl.java
+++ app/src/main/java/com/example/skincancer/viewmodel/Ocl.java
@@ -11,8 +11,14 @@ package com.example.skincancer.viewmodel; // Place in the relevant package direc
 /* OCL library for Java version 8+ */
 
 import java.util.ArrayList;
+import java.util.List;
 
-  public class Ocl {
+public class Ocl {
+ 
+ private Ocl() {
+        // Private constructor to prevent instantiation
+ }
+ 
  public static List<String> tokeniseCSV(String line)  {
    // Assumes the separator is a comma
    StringBuilder buff = new StringBuilder();
